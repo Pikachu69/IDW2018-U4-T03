bad0aa9cf806f00e2d64c20aa9ae421caa2c4757
Eduardo <lalitro.es@gmail.com>
2018-07-11 02:55:51 -0600

    Ejercicio 3

--- blogMain.js
+++ blogMain.js
@@ -5,3 +5,37 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/alumnos',{useNewUrlParser:true});
 
 var Blog = mongoose.model('Blog',blogSchema,"blogs");
+
+//Agregar blog
+/*
+var blog1 = new Blog({
+    title:'Hasta el moño del orgullo',
+    author:'Sarah Brenan',
+    body:'Decía una amiga de mi madre muy estilosona que el verano es la época más hortera del año.',
+    comments:[{body:'Regular', date:'1/07/2018'}],
+    meta: {votes:3, favs:4}
+})
+*/
+
+//Guardar
+/*
+blog1.save((error) => {
+    if(error){
+        console.log(error);
+        process.exit(1);
+    }
+    console.log("Guardado");
+    process.exit(0);
+})
+*/
+
+//Consulta por autor
+Blog.find({author:'Sarah Brenan'}, (error, docs) => {
+    if(error){
+        console.log(error);
+        process.exit(1);
+    }
+    console.log("<------- Consulta por Autor------->");
+    console.log(docs);
+    process.exit(0);
+});
